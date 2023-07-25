c7fa371c37aed036e010b553200b1c91ba97ffaa
saurabh rajpoot <saurabh941193@gmail.com>
2023-07-25 23:46:55 +0530

    this is final commit

---
+++ src/components/postview.jsx
@@ -0,0 +1,80 @@
+import "./app.css";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import SendIcon from "@mui/icons-material/Send";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const Postview = () => {
+  const [arr, setArr] = useState([]);
+  const [gif, setGif] = useState(true);
+
+  useEffect(() => {
+    setGif(true);
+    axios
+      .get("https://instaclone-backend-b997.onrender.com/posts")
+      .then(function (response) {
+        console.log(response);
+        setArr(response.data);
+        setGif(false);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
+  if (gif) {
+    return (
+      <>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+          }}
+        >
+          <img src={require("./Images/load.gif")} alt="load" />
+        </div>
+      </>
+    );
+  }
+  return (
+    <>
+      <div className="main">
+        
+        {arr.map((data, index) => {
+          return (
+            <>
+              <section className="container" key={index}>
+                <div className="sp-1">
+                  <div className="min-nav">
+                    <h4 className="name">{data.name}</h4>
+                    <MoreHorizIcon />
+                  </div>
+                  <p className="med">{data.location}</p>
+                </div>
+
+                <img className="img" src={data.PostImage} alt="" />
+                <div className="sp2">
+                  <div className="likesend">
+                    <FavoriteBorderIcon />
+                    <SendIcon />
+                  </div>
+                  <p className="med">{data.Date}</p>
+                </div>
+                <div className="sp3">
+                  <p className="med">{data.likes} Likes</p>
+                  <h4 className="name">{data.description}</h4>
+                </div>
+              </section>
+            </>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+export default Postview;
---
+++ src/components/upload.jsx
@@ -0,0 +1,195 @@
+import "./app.css";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import GifLoader from 'react-gif-loader';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+const Upload = () => {
+  const [image, setImage] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [imgurl, setImgurl] = useState("");
+  const [fileName, setFileName] = useState({
+    name: "",
+    description: "",
+    location: "",
+    PostImage: null,
+  });
+  const [error, setError] = useState({});
+
+  const nav = useNavigate();
+
+  useEffect(() => {
+    if (imgurl) {
+      
+      fetch("https://instaclone-backend-b997.onrender.com/posts", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          PostImage: imgurl,
+          name: fileName.name,
+          description: fileName.description,
+          location: fileName.location,
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.error) {
+            setError(data.error);
+          } else {
+             toast.success('Post updated successfully!', {
+              position: 'top-center',
+              autoClose: 2000, // Duration for which the message will be shown (in milliseconds)
+              hideProgressBar: true,
+              closeOnClick: true,
+              pauseOnHover: false,
+              draggable: true,
+            });
+            nav("/postview");
+          }
+        })
+        .catch((e) => console.log(e));
+    }
+  }, [imgurl]);
+
+  const ImageUpload = (event) => {
+    event.preventDefault();
+    const data = new FormData();
+    data.append("file", image);
+    data.append("upload_preset", "images");
+    data.append("cloud_name", "neeleshks");
+    fetch("https://api.cloudinary.com/v1_1/neeleshks/image/upload", {
+      method: "post",
+      body: data,
+    })
+      .then((res) => res.json())
+      .then((data) => setImgurl(data.secure_url))
+      .catch((e) => console.log(e));
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    if (!fileName.PostImage) {
+      setError({ ...error, PostImage: "Photo is required" });
+      return;
+    } else if (!fileName.name) {
+      setError({ ...error, name: "Author field is required" });
+      return;
+    } else if (!fileName.location) {
+      setError({ ...error, location: "Location field is required" });
+      return;
+    } else if (!fileName.description) {
+      setError({ ...error, description: "Description field is required" });
+      return;
+    }
+    setIsSubmitting(true);
+    ImageUpload(event);
+  };
+if(isSubmitting){
+  return(
+    <>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+          }}
+        >
+          <img src={require("./Images/load.gif")} alt="load" />
+        </div>
+      </>
+  )
+}
+  return (
+    <>
+      <div className="formarea">
+        
+        <div className="form">
+          <form onSubmit={handleFormSubmit}>
+            <div className="brwse">
+              <input
+                type="text"
+                id="choose"
+                value={fileName.PostImage ? fileName.PostImage.name : ""}
+                placeholder={
+                  fileName.PostImage
+                    ? fileName.PostImage.name
+                    : "No file selected"
+                }
+                readOnly
+              />
+              <label htmlFor="file" className="Browse">
+                Browse
+              </label>
+              <input
+                type="file"
+                id="file"
+                accept="image/*"
+                onChange={(e) => {
+                  setImage(e.target.files[0]);
+                  setFileName({ ...fileName, PostImage: e.target.files[0] });
+                  setError({ ...error, PostImage: "" });
+                }}
+              />
+              {error.PostImage && <h4 className="req">{error.PostImage}</h4>}
+            </div>
+
+            <br />
+            <div className="auth">
+              <input
+                type="text"
+                placeholder="Author"
+                name="name"
+                className="loca"
+                value={fileName.name}
+                onChange={(e) => {
+                  setFileName({ ...fileName, name: e.target.value });
+                  setError({ ...error, name: false });
+                }}
+              />
+              {error.name && <h4 className="req">*Author field is required</h4>}
+              <input
+                type="text"
+                placeholder="Location"
+                name="location"
+                value={fileName.location}
+                onChange={(e) =>
+                  setFileName({ ...fileName, location: e.target.value })
+                }
+                className="loca"
+              />
+              {error.location && (
+                <h4 className="req">*Location field is required</h4>
+              )}
+            </div>
+            <br />
+            <textarea
+              placeholder="Description"
+              name="description"
+              value={fileName.description}
+              onChange={(e) =>
+                setFileName({ ...fileName, description: e.target.value })
+              }
+              id="description"
+            />
+            {error.description && (
+              <h4 className="req">*Description field is required</h4>
+            )}
+            <div className="post-btn">
+              <button id="post" type="submit">
+                Post
+              </button>
+            </div>
+          </form>
+          
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Upload;
